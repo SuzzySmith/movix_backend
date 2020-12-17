30aa065549a8852d46c6fb4660349feae290edc2
darneltaphil <darneltaphil@gmail.com>
2020-12-17 16:23:10 +0000

    Getting started

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
-const db = require('./db')
-const movieRouter = require('./routes/movie-router')
 
 const app = express()
 const apiPort = 3000
@@ -12,12 +10,10 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get('/', (req, res) => {
     res.send('I am listening')
 })
 
-app.use('/api', movieRouter)
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
